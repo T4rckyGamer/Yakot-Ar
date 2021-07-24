612ac0bca8bd9a0d63574c7dfe2aa3a8f8c987b6
T4rckyGamer <68791309+T4rckyGamer@users.noreply.github.com>
2021-07-24 06:03:52 +0300

    Add files via upload

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -1,1 +1,81 @@
+import time
 
+import heroku3
+
+from .Config import Config
+from .core.logger import logging
+from .core.session import jmthon
+from .sql_helper.globals import addgvar, delgvar, gvarstatus
+
+__version__ = "0.1 beta"
+__license__ = "كـتابة وتـعديل فريـق ياقوت"
+__author__ = "تواصل المطور <https://T.ME/PROTECMnbot>"
+__copyright__ = "YAKOT TEAM (C) 2020 - 2021  " + __author__
+
+jmthon.version = __version__
+jmthon.tgbot.version = __version__
+LOGS = logging.getLogger("ياقوت")
+bot = jmthon
+
+StartTime = time.time()
+catversion = "0.1 beta"
+
+if Config.UPSTREAM_REPO == "Yakot-Ar":
+    UPSTREAM_REPO_URL = "https://github.com/T4rckyGamer/Yakot-Ar"
+elif Config.UPSTREAM_REPO == "badcat":
+    UPSTREAM_REPO_URL = "https://github.com/sandy1709/catuserbot"
+else:
+    UPSTREAM_REPO_URL = Config.UPSTREAM_REPO
+
+if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+    if gvarstatus("PRIVATE_GROUP_BOT_API_ID") is None:
+        Config.BOTLOG = False
+        Config.BOTLOG_CHATID = "me"
+    else:
+        Config.BOTLOG_CHATID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.PRIVATE_GROUP_BOT_API_ID = int(gvarstatus("PRIVATE_GROUP_BOT_API_ID"))
+        Config.BOTLOG = True
+else:
+    if str(Config.PRIVATE_GROUP_BOT_API_ID)[0] != "-":
+        Config.BOTLOG_CHATID = int("-" + str(Config.PRIVATE_GROUP_BOT_API_ID))
+    else:
+        Config.BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+    Config.BOTLOG = True
+
+if Config.PM_LOGGER_GROUP_ID == 0:
+    if gvarstatus("PM_LOGGER_GROUP_ID") is None:
+        Config.PM_LOGGER_GROUP_ID = -100
+    else:
+        Config.PM_LOGGER_GROUP_ID = int(gvarstatus("PM_LOGGER_GROUP_ID"))
+elif str(Config.PM_LOGGER_GROUP_ID)[0] != "-":
+    Config.PM_LOGGER_GROUP_ID = int("-" + str(Config.PM_LOGGER_GROUP_ID))
+
+try:
+    if Config.HEROKU_API_KEY is not None or Config.HEROKU_APP_NAME is not None:
+        HEROKU_APP = heroku3.from_key(Config.HEROKU_API_KEY).apps()[
+            Config.HEROKU_APP_NAME
+        ]
+    else:
+        HEROKU_APP = None
+except Exception:
+    HEROKU_APP = None
+
+
+# Global Configiables
+COUNT_MSG = 0
+USERS = {}
+COUNT_PM = {}
+LASTMSG = {}
+CMD_HELP = {}
+ISAFK = False
+AFKREASON = None
+CMD_LIST = {}
+SUDO_LIST = {}
+# for later purposes
+INT_PLUG = ""
+LOAD_PLUG = {}
+
+# Variables
+BOTLOG = Config.BOTLOG
+BOTLOG_CHATID = Config.BOTLOG_CHATID
+PM_LOGGER_GROUP_ID = Config.PM_LOGGER_GROUP_ID
